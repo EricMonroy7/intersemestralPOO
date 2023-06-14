5ca8280cfd1177808bdbde6760b39ab92bd87d9b
EricMonroy7 <111954674+EricMonroy7@users.noreply.github.com>
2023-06-13 23:48:25 -0600

    Clase Martes POO

---
+++ mainPOO.py
@@ -0,0 +1,26 @@
+
+
+def main():
+    al1 = Alumno()
+    print(al1)
+
+main()
+
+print
+al1.facultad = "FES Aragon EDOMEX"
+print(al1. facultad)
+print(al2. facultad)
+print(Alumno. facultad)
+print("----Un vistazzo a los objetos")
+print(vars (al1))
+print(vars(al2))
+
+
+al1 = Alumno ("Diego", 19, "ICO")
+al2 = Alumno("Montse", 20, "Derecho")
+print(al1.nombre)
+print(al1.facultad)
+print(a12.nombre)
+print(al2.facut.tad)
+
+main()
