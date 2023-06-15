f4aad3ad3c41a18159e63a068ab7b51b177297bd
EricMonroy7 <111954674+EricMonroy7@users.noreply.github.com>
2023-06-15 00:04:24 -0600

    Add files via upload

---
+++ martesPOO/cosas.py
@@ -0,0 +1,12 @@
+class Alumno:
+    facultad = "FES Aragón"
+
+    def __init__(self):
+        self, nombre = "Juan"
+        self.carrera = "Ingeniería en Connetación"
+
+
+all = ALumno
+print(all.nombre)
+print(al1.carrera)
+print(all.facultad)
